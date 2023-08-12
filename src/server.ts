8c23a6e7055780ea5e47372ea59800f9ca8bad21
Pavel Dubrovskiy <paveldubrovskiyit@gmail.com>
2023-08-12 23:54:13 +0300

    Basic project structure implemented

---
+++ src/server.ts
@@ -0,0 +1,12 @@
+import express from "express";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const app = express();
+
+const PORT = Number(process.env.PORT) || 3000;
+
+app.listen(PORT, () => {
+  console.log(`[server]: Server is running at http://localhost:${PORT}`);
+});
