f3a9121005846d7a2a4b9c88116c6b7cd42d961d
Pavel Dubrovskiy <paveldubrovskiyit@gmail.com>
2023-08-13 23:24:55 +0300

    Error-handling middleware implemented

--- src/server.ts
+++ src/server.ts
@@ -1,5 +1,6 @@
-import express from "express";
-import dotenv from "dotenv";
+import express, { Request, Response } from 'express';
+import dotenv from 'dotenv';
+import createPath from './common/createPath';
 
 dotenv.config();
 
@@ -9,4 +10,10 @@ const PORT = Number(process.env.PORT) || 3000;
 
 app.listen(PORT, () => {
   console.log(`[server]: Server is running at http://localhost:${PORT}`);
+});
+
+app.set('view engine', 'ejs');
+
+app.use((req: Request, res: Response) => {
+  res.status(404).render(createPath('error'), { title: 'Error' });
 });
